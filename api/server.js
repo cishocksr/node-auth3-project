a9f29bdbc7bd0457477ddd345eb59f7138aff08b
Christopher Shockley <cishockleysr@gmail.com>
2020-02-20 20:07:44 -0500

    inital commit/server running port 5000

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(morgan());
+server.use(express.json());
+server.use(cors());
+
+server.use((err, req, res, next) => {
+  return res.status(500).json({
+    message: 'Something went wrong'
+  });
+});
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'I am running...'
+  });
+});
+
+module.exports = server;
